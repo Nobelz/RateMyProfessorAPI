0d89a9d3d50a1381e78b4476a5af4b41cceaae73
Nobelium <nobelzhou19@gmail.com>
2020-12-17 19:19:05 -0500

    project structure setup

---
+++ ratemyprofessor/__init__.py
@@ -0,0 +1,12 @@
+"""
+ratemyprofessor
+
+RateMyProfessor API Wrapper
+An extremely basic wrapper for the RateMyProfessorAPI.
+
+:copyright: (c) 2020 Nobelz
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+from .professor import *
+from .school import *

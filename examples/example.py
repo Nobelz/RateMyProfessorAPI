490e52b351f25fc8f3ba831e0839d44b66868c0d
Nobel Zhou <nobelzhou19@gmail.com>
2023-06-07 14:40:45 -0400

    Semantic adjustments in example.py

--- examples/example.py
+++ examples/example.py
@@ -3,9 +3,9 @@ import ratemyprofessor
 professor = ratemyprofessor.get_professor_by_school_and_name(
     ratemyprofessor.get_school_by_name("Case Western Reserve University"), "Connamacher")
 if professor is not None:
-    print("%sworks in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
-    print("Rating: %s / 5.0" % professor.rating)
-    print("Difficulty: %s / 5.0" % professor.difficulty)
+    print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
+    print("Rating: %s / 5" % professor.rating)
+    print("Difficulty: %s / 5" % professor.difficulty)
     print("Total Ratings: %s" % professor.num_ratings)
     if professor.would_take_again is not None:
         print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
